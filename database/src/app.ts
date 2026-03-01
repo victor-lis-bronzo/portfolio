4c16b581fedb89a2b9d2494b75a5855462b20a54
Victor-Lis <victorlisbronzo1@gmail.com>
2026-03-01 16:52:53 -0300

    feat: Add Drizzle ORM database module and configure PostgreSQL with environment variables.

---
+++ database/src/app.ts
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+
+const connectionString = process.env.DATABASE_URL!;
+
+const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client);
