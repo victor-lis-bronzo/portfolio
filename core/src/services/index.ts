cb81b8649787e082d6817c78dd60057b7ee2eb05
Victor-Lis <victorlisbronzo1@gmail.com>
2026-03-25 10:57:02 -0300

    feat(core): export services from root index

--- core/src/services/index.ts
+++ core/src/services/index.ts
@@ -2,13 +2,16 @@ import { PostsService } from "./posts.service";
 import { ProjectsService } from "./projects.service";
 import { UsersService } from "./users.service";
 import { TechsService } from "./techs.service";
+import { AuthService } from "./auth.service";
 
 export const postsService = new PostsService();
 export const projectsService = new ProjectsService();
 export const usersService = new UsersService();
 export const techsService = new TechsService();
+export const authService = new AuthService();
 
 export * from "./posts.service";
 export * from "./projects.service";
 export * from "./users.service";
 export * from "./techs.service";
+export * from "./auth.service";
