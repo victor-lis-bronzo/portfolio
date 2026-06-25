848698bcb92b6fbddd126152706191b12f4acb11
Victor-Lis <victorlisbronzo1@gmail.com>
2026-06-25 11:41:55 -0300

    feat(drawmylife): add splitText utility to break long lines of text

---
+++ web/src/app/drawmylife/utils/split-text.ts
@@ -0,0 +1,25 @@
+export function splitText(text: string, wordsCount: number = 9): string {
+  if (!text) return "";
+
+  const response = text
+    .split("\n")
+    .map((line) => {
+      const words = line.split(" ").filter(Boolean);
+      let lineResult = "";
+
+      for (let i = 0; i < words.length; i++) {
+        lineResult += words[i];
+        if (i < words.length - 1) {
+          if ((i + 1) % wordsCount === 0) {
+            lineResult += "\n";
+          } else {
+            lineResult += " ";
+          }
+        }
+      }
+
+      return lineResult;
+    })
+    .join("\n");
+  return response;
+}
