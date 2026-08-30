b47057ec8d1e683f021ef06d7d01a4aa0135d508
Victor-Lis <victorlisbronzo1@gmail.com>
2026-08-30 18:06:14 -0300

    fix(types): update EVENTS_BOARD label to use Localized type instead of string

--- src/core/entities/scene-waypoint-config.ts
+++ src/core/entities/scene-waypoint-config.ts
@@ -67,6 +67,6 @@ export const SCENE_WAYPOINTS: SceneWaypoint[] = [
 		position: { x: 0, y: 4.8, z: 4.0 },
 		target: { x: -7.8, y: 1.8, z: 4.0 },
 		zoom: 100,
-		label: "Eventos Tech",
+		label: { en: "Tech Events", pt: "Eventos Tech" },
 	},
 ];
